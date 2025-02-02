f251226bbea16714963b3c825fcc0e7b9cad49a0
pawelzwronek <pawel.z.wronek@gmail.com>
2025-02-02 18:53:54 +0100

    Rebuild PyInstaller bootloader with MinGW64 to fix #5 - a false-positive Windows Defender alert (Windows only)

--- venv-prepare.cmd
+++ venv-prepare.cmd
@@ -3,3 +3,4 @@ set OS=win
 python -m venv venv-%OS%
 call venv-%OS%\Scripts\activate.bat
 pip install -r requirements-win.txt
+copy /Y bin\pyinstaller\bootloader\*.exe venv-%OS%\Lib\site-packages\PyInstaller\bootloader\Windows-64bit-intel
